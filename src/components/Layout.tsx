804431ce12c0d7d4e0a36076a70433b4fbc78109
montassar.kebsi <kebsimontassar@gmail.com>
2025-10-31 01:19:27 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -322,7 +322,7 @@ const Layout = ({ children }: LayoutProps) => {
                 borderColor={colors.borderSubtle}
               >
                 <Link
-                  href="https://github.com/KabsiMontassar/Valero"
+                  href="https://github.com/KabsiMontassar/ValeroChatAPP"
                   target="_blank"
                   rel="noopener noreferrer"
                   _hover={{ textDecoration: 'none' }}
